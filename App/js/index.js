a91016dca7ba58f7b28489c86df7f104763638ce
Allen83219 <68113959+Allen83219@users.noreply.github.com>
2020-09-12 17:36:13 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,61 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel4")
+                .setDock("none")
+                .setLeft("0em")
+                .setTop("0em")
+                .setWidth("49.98095238095238em")
+                .setHeight("30.17142857142857em")
+                .setCaption("Foldable Panel")
+                .setToggleBtn(true)
+                .setCloseBtn(true)
+                .setRefreshBtn(true)
+            );
+            
+            host.xui_ui_panel4.append(
+                xui.create("xui.UI.ProgressBar")
+                .setHost(host,"xui_ui_progressbar2")
+                .setDirtyMark(false)
+                .setLeft("23.16190476190476em")
+                .setTop("1.7523809523809524em")
+                .setWidth("2em")
+                .setHeight("46.7047619047619em")
+                .setType("vertical")
+                .setRotate(90)
+            );
+            
+            append(
+                xui.create("xui.UI.PopMenu")
+                .setHost(host,"xui_ui_popmenu6")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"item 1",
+                        "imageClass":"xui-icon-number1"
+                    },
+                    {
+                        "id":"b",
+                        "caption":"item 2",
+                        "imageClass":"xui-icon-number2"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"item 3",
+                        "imageClass":"xui-icon-number3"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"item 4",
+                        "imageClass":"xui-icon-number4",
+                        "disabled":true
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
@@ -28,7 +82,8 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
-        }
+        },
+        functions:{ }
         /*,
         // To determine how properties affects this module
         propSetAction : function(prop){
